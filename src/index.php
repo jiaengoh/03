06b837dece477fa78a5f2184639374bf58a32d45
jiaengoh <57712490+jiaengoh@users.noreply.github.com>
2021-12-02 14:40:50 +0800

    update

--- src/index.php
+++ src/index.php
@@ -1,7 +1,22 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+    function test_input($data){
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    $search = test_input($_POST["search"]);
+
+?>
+
 <html>
 <body>
 
-<form action="search.php" method="get">
+<form action="search.php" method="post">
 search <input type="text" name="search"><br>
 
 <input type="submit">Search</input>
@@ -9,3 +24,5 @@ search <input type="text" name="search"><br>
 
 </body>
 </html>
+
+<script language="Javascript">alert("Hello");</script>
